106040bbe7191416e6e310f3de8366b7b63c3cc8
Maria Deslis <mdeslis@gmail.com>
2018-10-12 13:46:23 -0600

    Able to connect and close two circuits
    a0 and a4 calue connections, basic ideas at the moment

--- closedCircuitNeoPixelsPlayDoh/closedCircuitNeoPixelsPlayDoh.ino
+++ closedCircuitNeoPixelsPlayDoh/closedCircuitNeoPixelsPlayDoh.ino
@@ -5,10 +5,15 @@ const int pixel_length = 10; //variable for number of LEDs in the neopixel strip
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(pixel_length, PIN, NEO_GRB + NEO_KHZ800); //define neopixel strip
 
+// Red lights
 int a0_val = 0; //variable that updates from analog reading pin A0
-
 int a0_min = 0; //minimum a0 cut off value
 int a0_max = 4095; //maximum a0 cut off value
+
+// Green lights
+int a4_val = 0; //variable that updates from analog reading pin A0
+int a4_min = 0; //minimum a0 cut off value
+int a4_max = 4095; //maximum a0 cut off value
 
 void setup() {
   analogReadResolution(12); //set analog read resolution of the microcontroller to 12 bits
@@ -20,20 +25,30 @@ void setup() {
 
 void loop() {
   a0_val = (a0_val + analogRead(A0)) / 2; //we analog read a0, add it to itself, and average it to smooth the signal (slightly)
-//  Serial.print("A0_VAL:");
-//  Serial.println(a0_val); //print a0_val to serial
+  Serial.print("A0_VAL:");
+  Serial.println(a0_val); //print a0_val to serial
   int led_count = map(a0_val, a0_min, a0_max, 0, pixel_length); //maps a0_val between min and max to the number of LEDs to light up
 //  Serial.print("LEDCOUNT: ");
 //  Serial.println(led_count);
   int step_size = (a0_max - a0_min)/pixel_length; //find the a0 value interval between each LED to light
 
+  // Get a4_val
+  a4_val = (a4_val + analogRead(A4)) / 2; //we analog read a0, add it to itself, and average it to smooth the signal (slightly)
+  Serial.print("A4_VAL:");
+  Serial.println(a4_val); //print a0_val to serial
+  int led_count4 = map(a4_val, a4_min, a4_max, 0, pixel_length); //maps a0_val between min and max to the number of LEDs to light up
+  int step_size4 = (a4_max - a4_min)/pixel_length; //find the a0 value interval between each LED to light
+
+
   //0 to 9, 0 being the first pixel in the list
   for (uint16_t i = 0; i < strip.numPixels(); i++) { //iterate through all the LEDs in the neopixel strip
-//    Serial.print("i: ");
-//    Serial.println(i);
     if (a0_val > 100) {
       // when the circuit closes light up all the neopixels randomly
-      strip.setPixelColor(i,random(255), random(255), random(255));
+      // when a0 val is active color all pixels yellow
+      strip.setPixelColor(i,255,255,0);
+    } else if (a4_val > 100) {
+      // when a4 is active color all pixels blue
+      strip.setPixelColor(i,0,0,255);
     } else {
       // turn them all off otherwise
       strip.setPixelColor(i,0,0,0);
--- processingResults/processingResults.pde
+++ processingResults/processingResults.pde
@@ -4,24 +4,31 @@
 import processing.serial.*;
 
 Serial myPort; // Create object from Serial class
-String val;    // Date received from the serial port
+String val;     // Data received from the serial port
 
 void setup()
 {
-  // I know that the first port in the serial list on my mac
-  // is Serial.list()[0].
-  // On Windows machines, this generally opens COM1.
-  // Open whatever port is the one you're using.
+  size(500,500);
+  background(255);
   String portName = Serial.list()[0]; //change the 0 to a 1 or 2 etc. to match your port
+  println("setup portName: ", portName);
   myPort = new Serial(this, portName, 9600);
+  println("myPort: ", myPort);
 }
 
-void draw()
-{
-  if (myPort.available() > 0)
-  {
-    val = myPort.readStringUntil('\n');
+void draw() {
+  println("Serial length: ", Serial.list().length);
+  for(int i = 0; i < Serial.list().length; i++) {
+    println("i :", i," PORT#: ", Serial.list()[i]);
   }
   
-  println(val);
+  if (mousePressed == true) 
+  {                           //if we clicked in the window
+   myPort.write('1');         //send a 1
+   println("1");   
+  } 
+  else 
+  {                           //otherwise
+    myPort.write('0');          //send a 0
+  }   
 }
