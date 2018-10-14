9edd1186850688f900d7e51c8c2b420691f62084
Maria Deslis <mdeslis@gmail.com>
2018-10-14 16:10:25 -0600

    Arduino talks to Processing
    Depending on which and how many circuits are closed, arduino will send a character to processing and processing will draw a circle with a color specific to that character

--- closedCircuitNeoPixelsPlayDoh/closedCircuitNeoPixelsPlayDoh.ino
+++ closedCircuitNeoPixelsPlayDoh/closedCircuitNeoPixelsPlayDoh.ino
@@ -25,39 +25,39 @@ void setup() {
 
 void loop() {
   a0_val = (a0_val + analogRead(A0)) / 2; //we analog read a0, add it to itself, and average it to smooth the signal (slightly)
-  Serial.print("A0_VAL:");
-  Serial.println(a0_val); //print a0_val to serial
   int led_count = map(a0_val, a0_min, a0_max, 0, pixel_length); //maps a0_val between min and max to the number of LEDs to light up
-//  Serial.print("LEDCOUNT: ");
-//  Serial.println(led_count);
   int step_size = (a0_max - a0_min)/pixel_length; //find the a0 value interval between each LED to light
 
   // Get a4_val
   a4_val = (a4_val + analogRead(A4)) / 2; //we analog read a0, add it to itself, and average it to smooth the signal (slightly)
-  Serial.print("A4_VAL:");
-  Serial.println(a4_val); //print a0_val to serial
   int led_count4 = map(a4_val, a4_min, a4_max, 0, pixel_length); //maps a0_val between min and max to the number of LEDs to light up
   int step_size4 = (a4_max - a4_min)/pixel_length; //find the a0 value interval between each LED to light
 
-
   //0 to 9, 0 being the first pixel in the list
   for (uint16_t i = 0; i < strip.numPixels(); i++) { //iterate through all the LEDs in the neopixel strip
+//    Serial.print("A0: ");
+//    Serial.println(a0_val);
+//    Serial.print("A4: ");
+//    Serial.println(a4_val);
     if (a0_val > 100 && a4_val > 100) {
       // when both are connected to close the circuit turn the pixels a different color
       // to indicate that all is working correctly
+      Serial.println('3');
       strip.setPixelColor(i,255,0,0);
     } else if (a4_val > 100) {
       // when a4 is active color all pixels blue
+      Serial.println('2');
       strip.setPixelColor(i,0,0,255);
     } else if (a0_val > 100) {
        // when the circuit closes light up all the neopixels randomly
-      // when a0 val is active color all pixels yellow
-      strip.setPixelColor(i,255,255,0);
+      Serial.println('1');
+      strip.setPixelColor(i,0,255,0);
     } else {
+      Serial.println('0');
       // turn them all off otherwise
       strip.setPixelColor(i,0,0,0);
     }
   }
   strip.show(); //update the strip colors
-  delay(100); //100ms delay
+  delay(500); 
 }
--- processingResults/processingResults.pde
+++ processingResults/processingResults.pde
@@ -4,31 +4,69 @@
 import processing.serial.*;
 
 Serial myPort; // Create object from Serial class
-String val;     // Data received from the serial port
+char val;     // Data received from the serial port
 
 void setup()
 {
-  size(500,500);
+  size(200,200);
   background(255);
   String portName = Serial.list()[0]; //change the 0 to a 1 or 2 etc. to match your port
-  println("setup portName: ", portName);
   myPort = new Serial(this, portName, 9600);
-  println("myPort: ", myPort);
 }
 
 void draw() {
-  println("Serial length: ", Serial.list().length);
-  for(int i = 0; i < Serial.list().length; i++) {
-    println("i :", i," PORT#: ", Serial.list()[i]);
-  }
+  noStroke();
+  //println("Serial length: ", Serial.list().length);
+  //for(int i = 0; i < Serial.list().length; i++) {
+  //  println("i :", i," PORT#: ", Serial.list()[i]);
+  //}
   
-  if (mousePressed == true) 
-  {                           //if we clicked in the window
-   myPort.write('1');         //send a 1
-   println("1");   
+  if ( myPort.available() > 0) 
+  {  
+    // If data is available,
+    val = myPort.readChar();         // read it and store it in val
   } 
-  else 
-  {                           //otherwise
-    myPort.write('0');          //send a 0
-  }   
+ 
+  switch(val) {
+    case '0':
+      drawBlankScreen();
+      println("Zero");
+      break;
+    case '1':
+      println("One");
+      drawGreenCircle();
+      break;
+    case '2':
+      println("Two");
+      drawBlueCircle();
+      break;
+    case '3':
+      println("Three");
+      drawRedCircle();
+      break;
+    default:  
+      println("Default");
+      break;
+  }
+
+}
+
+void drawRedCircle() {
+  fill(255, 0, 0);
+  ellipse(width/2, height/2, 100, 100); 
+}
+
+void drawGreenCircle() {
+  fill(0,255,0);
+  ellipse(width/2, height/2, 100, 100);
+}
+
+void drawBlueCircle() {
+ fill(0,0,255);
+ ellipse(width/2, height/2, 100, 100);
+}
+
+void drawBlankScreen() {
+  fill(255);
+  rect(0,0,width,height);
 }
