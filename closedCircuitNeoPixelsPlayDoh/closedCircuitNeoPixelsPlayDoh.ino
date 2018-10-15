5e513bc82936d75b8c0a845827b99af511c0d321
Maria Deslis <mdeslis@gmail.com>
2018-10-15 12:20:02 -0600

    add A2 as fifth input

--- closedCircuitNeoPixelsPlayDoh/closedCircuitNeoPixelsPlayDoh.ino
+++ closedCircuitNeoPixelsPlayDoh/closedCircuitNeoPixelsPlayDoh.ino
@@ -13,6 +13,10 @@ int a1_val = 0; //variable that updates from analog reading pin A0
 int a1_min = 0; //minimum a0 cut off value
 int a1_max = 4095; //maximum a0 cut off value
 
+int a2_val = 0; //variable that updates from analog reading pin A0
+int a2_min = 0; //minimum a0 cut off value
+int a2_max = 4095; //maximum a0 cut off value
+
 int a3_val = 0; //variable that updates from analog reading pin A0
 int a3_min = 0; //minimum a0 cut off value
 int a3_max = 4095; //maximum a0 cut off value
@@ -38,6 +42,11 @@ void loop() {
   int led_count1 = map(a1_val, a1_min, a1_max, 0, pixel_length); //maps a0_val between min and max to the number of LEDs to light up
   int step_size1 = (a1_max - a1_min)/pixel_length; //find the a0 value interval between each LED to light
 
+  a2_val = (a2_val + analogRead(A2)) / 2; //we analog read a0, add it to itself, and average it to smooth the signal (slightly)
+  int led_count2 = map(a2_val, a2_min, a2_max, 0, pixel_length); //maps a0_val between min and max to the number of LEDs to light up
+  int step_size2 = (a2_max - a2_min)/pixel_length; //find the a0 value interval between each LED to light
+
+
   a3_val = (a3_val + analogRead(A3)) / 2; //we analog read a0, add it to itself, and average it to smooth the signal (slightly)
   int led_count3 = map(a3_val, a3_min, a3_max, 0, pixel_length); //maps a0_val between min and max to the number of LEDs to light up
   int step_size3 = (a3_max - a3_min)/pixel_length; //find the a0 value interval between each LED to light
@@ -47,15 +56,16 @@ void loop() {
   int led_count4 = map(a4_val, a4_min, a4_max, 0, pixel_length); //maps a0_val between min and max to the number of LEDs to light up
   int step_size4 = (a4_max - a4_min)/pixel_length; //find the a0 value interval between each LED to light
 
-  Serial.print("A0: ");
-  Serial.println(a0_val);
-   Serial.print("A1: ");
-  Serial.println(a1_val);
-  
-  Serial.print("A3: ");
-  Serial.println(a3_val);
-  Serial.print("A4: ");
-  Serial.println(a4_val);
+//  Serial.print("A0: ");
+//  Serial.println(a0_val);
+//   Serial.print("A1: ");
+//  Serial.println(a1_val);
+//  Serial.print("A2: ");
+//  Serial.println(a2_val);  
+//  Serial.print("A3: ");
+//  Serial.println(a3_val);
+//  Serial.print("A4: ");
+//  Serial.println(a4_val);
 
 
   //0 to 9, 0 being the first pixel in the list
@@ -63,26 +73,30 @@ void loop() {
     if (a0_val > 4000 && a4_val > 4000) {
       // when both are connected to close the circuit turn the pixels a different color
       // to indicate that all is working correctly
-//      Serial.println('5');
+      Serial.println('5');
       strip.setPixelColor(i,255,0,0);
     } else if (a4_val > 4000) {
       // kidney stone is yellow when lit
-//      Serial.println('2');
+      Serial.println('4');
       strip.setPixelColor(i,255,255,0);
     } else if (a3_val > 3000) {
       // A3 lung cancer is green
-//      Serial.println('3');
+      Serial.println('3');
       strip.setPixelColor(i,0,255,0);
+    } else if (a2_val > 4000) {
+      // brain tumor disk is purple
+      Serial.println('1');
+      strip.setPixelColor(i,186,85,211);
     } else if (a1_val > 4000) {
       // hernieted disk is blue
-//      Serial.println('3');
+      Serial.println('1');
       strip.setPixelColor(i,0,0,255);
     } else if (a0_val > 3000) {
        // blood clot is red when only it is lit
-//      Serial.println('1');
+      Serial.println('0');
       strip.setPixelColor(i,255,0,0);
     } else {
-//      Serial.println('0');
+      Serial.println('6');
       // turn them all off otherwise
       strip.setPixelColor(i,0,0,0);
     }
